2beea40deb8fba193cc4df323f0aca5b6a9f6c09
Matěj Chalk <matej.chalk@flowup.cz>
2024-09-13 15:14:12 +0200

    Run merge-diffs command in project where code-pushup is installed

--- src/cli/context.ts
+++ src/cli/context.ts
@@ -1,8 +1,22 @@
 import type { ActionInputs } from '../inputs'
+import type { ProjectConfig } from '../monorepo'
 
 export type CommandContext = Pick<
   ActionInputs,
   'bin' | 'config' | 'directory' | 'silent'
 > & {
   project?: string
+}
+
+export function createCommandContext(
+  inputs: ActionInputs,
+  project: ProjectConfig | null | undefined
+): CommandContext {
+  return {
+    project: project?.name,
+    bin: project?.bin ?? inputs.bin,
+    directory: project?.directory ?? inputs.directory,
+    config: inputs.config,
+    silent: inputs.silent
+  }
 }
